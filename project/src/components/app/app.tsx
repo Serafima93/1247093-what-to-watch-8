f7bdb79c2567756102cfabc8323ce4fe7dee7548
Serafima <serafima93.93@gmail.com>
2021-10-02 16:36:38 +0300

    Разбивка приложения на компоненты

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,23 @@
-function App(): JSX.Element {
-  return <p>Hello, world Sima!</p>;
+import MainPage from '../main/main';
+
+type AppScreenProps = {
+  filmsCount: number[];
+  filmName: string;
+  filmType: string;
+  filmDate: number;
+};
+
+function App(props: AppScreenProps): JSX.Element {
+  const { filmsCount, filmName, filmType, filmDate } = props;
+
+  return (
+    <MainPage
+      filmsCount={filmsCount}
+      filmName={filmName}
+      filmType={filmType}
+      filmDate={filmDate}
+    />
+  );
 }
 
 export default App;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+// я же в дальнейшем буду получать массив объектов фильмов поэтому сделала так
+const FilmsCardsCount = {
+  FILMS_COUNT: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+};
+
+const FilmDescription = {
+  filmName: 'The Grand Budapest Hotel',
+  filmType: 'Drama',
+  filmDate: 2014,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* я понимаю что это слишком длинно, но не знаю как сделать это короче */}
+    <App
+      filmsCount={FilmsCardsCount.FILMS_COUNT}
+      filmName={FilmDescription.filmName}
+      filmType={FilmDescription.filmType}
+      filmDate={FilmDescription.filmDate}
+    />
   </React.StrictMode>,
-  document.getElementById('root'));
+  document.getElementById('root'),
+);
